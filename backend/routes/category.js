8791d8b2c18990b2b7b999ce8776233886714186
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-04-19 18:16:19 +0530

    fixing s3 issues of form data

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const { categoryCreateValidator } = require('../validators/category');
-const { runValidation } = require('../validators');
-const { verifyToken, authMiddleware } = require('../controllers/auth');
-const { create, list, read } = require('../controllers/category');
+const express = require("express");
+const { categoryCreateValidator } = require("../validators/category");
+const { runValidation } = require("../validators");
+const { verifyToken, authMiddleware } = require("../controllers/auth");
+const { create, list, read } = require("../controllers/category");
 const router = express.Router();
 
-router.post('/category',
-// categoryCreateValidator,runValidation, this works only for body data not for form data
-verifyToken,authMiddleware,create )
-router.get('/categories', list);
-router.post('/category/:slug', read);
+router.post(
+	"/category",
+	// categoryCreateValidator,runValidation, this works only for body data not for form data
+	verifyToken,
+	authMiddleware,
+	create,
+);
+router.get("/categories", list);
+router.post("/category/:slug", read);
 
-
-
-router.get("/cat",(req,res)=>{
-    res.send("Category")
-})
-module.exports = router; 
+router.get("/cat", (req, res) => {
+	res.send("Category");
+});
+module.exports = router;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,58 +1,53 @@
-import Navbar from '@/components/Navbar'
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useState} from 'react'
+import Navbar from "@/components/Navbar";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
+const Home = ({ categories }) => {
+	const listCategories = () => {
+		return categories.map((c, i) => (
+			<a key={i} href={`/links/${c.slug}`}>
+				<a className='border border-red-500 bg-gray-100 p-3 flex flex-col col-span-1 md:col-span-4'>
+					<div className='flex flex-row'>
+						<div className='flex flex-col'>
+							<img
+								src={c.image && c.image.url}
+								alt={c.name}
+								className='w-24 h-auto'
+							/>
+						</div>
+						<div className='col-span-1'>
+							<h3 className='text-xl'>{c.name}</h3>
+						</div>
+					</div>
+				</a>
+			</a>
+		));
+	};
+	const [domLoaded, setDomLoaded] = useState(false);
+	useEffect(() => {
+		setDomLoaded(true);
+	}, []);
+	return (
+		<div>
+			<Navbar>
+				<br />
+				<br />
 
-const Home = ({categories}) => {
-  const listCategories=()=>{
-    return(
-    categories.map((c, i) => (
-      <a key={i} href={`/links/${c.slug}`}>
-          <a className="border border-red-500 bg-gray-100 p-3 flex flex-col col-span-1 md:col-span-4">
-              <div className="flex flex-row">
-                  <div className="flex flex-col">
-                      <img
-                          src={c.image && c.image.url}
-                          alt={c.name} 
-                          className="w-24 h-auto"
-                      />
-                  </div>
-                  <div className="col-span-1">
-                      <h3 className="text-xl">{c.name}</h3>
-                  </div>
-              </div>
-          </a>
-      </a>)
-  ));
-  
-  }
-  const [domLoaded, setDomLoaded] = useState(false);
-  useEffect(() => {
-    setDomLoaded(true)
-  }, []);
-  return (
-    <div>
-      <Navbar>
-        <br/>
-        <br/>
+				{JSON.stringify(categories)}
 
-
-        {JSON.stringify(categories)}
-
-        <div className="flex flex-row">{listCategories()}</div>
-
-      </Navbar>
-    </div>
-  )
-}
+				<div className='flex flex-row'>{listCategories()}</div>
+			</Navbar>
+		</div>
+	);
+};
 Home.getInitialProps = async () => {
-  const response=await axios.get('http://localhost:5000/api/categories');
-  // const response={};
-  console.log(response.data)
-  return {
-    categories:response.data 
-  }
-}
+	const response = await axios.get("http://localhost:5000/api/categories");
+	// const response={};
+	console.log(response.data);
+	return {
+		categories: response.data,
+	};
+};
 
-export default Home
+export default Home;
